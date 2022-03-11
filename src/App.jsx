da310bc2b7e85bf34f7f3d99eeeea7ed1d4e2f0a
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-03-11 22:44:49 +0500

    It's Time To Push !

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Paper from './Components/Paper';
+
+function App() {
+  return (
+    <div className="App">
+      <Paper />
+    </div>
+  );
+}
+
+export default App;
